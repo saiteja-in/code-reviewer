2a979fce3862d9707c513ea3909600d35fa8c40a
saiteja-in <vurukondasaiteja13@gmail.com>
2026-06-06 13:17:31 +0530

    setup trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import { HealthCheck } from "@/components/health-check";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Button>hello there</Button>
+    <div className="flex min-h-screen items-center justify-center">
+      <div>
+        <h1>welcome to ai code reviewer.</h1>
+        <p>start reviewing your code now.</p>
+
+      </div>
+      <div className="flex gap-4">
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/login">Logout</Link>
+        </Button>
+      </div>
+      <HealthCheck/>
+    </div>
   );
 }
